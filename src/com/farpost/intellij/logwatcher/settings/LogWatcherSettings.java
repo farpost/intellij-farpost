329ca3a1ee8e621440c76b96e92b02e9c99aecf6
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-10-15 13:19:34 +1100

    Add simple implementation of settings service

---
+++ src/com/farpost/intellij/logwatcher/settings/LogWatcherSettings.java
@@ -0,0 +1,35 @@
+package com.farpost.intellij.logwatcher.settings;
+
+import com.intellij.openapi.components.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@State(name = "LogWatcher", storages = {@Storage(file = StoragePathMacros.PROJECT_CONFIG_DIR + "/logwatcher.xml")})
+public class LogWatcherSettings implements PersistentStateComponent<LogWatcherSettings> {
+  private String url = "";
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public static LogWatcherSettings getInstance(@NotNull Project project) {
+    return ServiceManager.getService(project, LogWatcherSettings.class);
+  }
+
+  @Nullable
+  @Override
+  public LogWatcherSettings getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(LogWatcherSettings state) {
+    XmlSerializerUtil.copyBean(state, this);
+  }
+}
