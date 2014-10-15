c5012bb876770b82e427c80d573f3246161d43bb
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-10-16 01:05:19 +1100

    Restart analyzer on host change

--- src/com/farpost/intellij/logwatcher/settings/LogWatcherConfigurable.java
+++ src/com/farpost/intellij/logwatcher/settings/LogWatcherConfigurable.java
@@ -1,20 +1,32 @@
 package com.farpost.intellij.logwatcher.settings;
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.project.Project;
 import com.intellij.ui.components.JBTextField;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class LogWatcherConfigurable implements SearchableConfigurable {
+  @NotNull private final Project myProject;
   @NotNull private final LogWatcherSettings mySettings;
   private JPanel myPanel;
   private JBTextField myHostField;
 
-  public LogWatcherConfigurable(@NotNull LogWatcherSettings settings) {
+  public LogWatcherConfigurable(@NotNull Project project, @NotNull LogWatcherSettings settings) {
+    myProject = project;
     mySettings = settings;
   }
 
@@ -32,6 +44,9 @@ public class LogWatcherConfigurable implements SearchableConfigurable {
   @Override
   public void apply() throws ConfigurationException {
     mySettings.setUrl(myHostField.getText());
+    
+    DaemonCodeAnalyzerImpl codeAnalyzer = (DaemonCodeAnalyzerImpl)DaemonCodeAnalyzer.getInstance(myProject);
+    codeAnalyzer.restart();
   }
 
   @Override
