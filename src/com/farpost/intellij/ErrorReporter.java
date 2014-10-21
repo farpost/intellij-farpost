74b93b42a4979ebc5d46c3dcf7748d1b282e3420
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-10-21 18:20:16 +1100

    Opitmize imports

--- src/com/farpost/intellij/ErrorReporter.java
+++ src/com/farpost/intellij/ErrorReporter.java
@@ -1,11 +1,9 @@
 package com.farpost.intellij;
 
 import com.intellij.diagnostic.ITNReporter;
-import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
 import com.intellij.openapi.diagnostic.SubmittedReportInfo;
 import com.intellij.util.Consumer;
-import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
